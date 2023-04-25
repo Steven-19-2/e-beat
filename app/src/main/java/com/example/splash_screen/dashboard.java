3d4313cca327d4ef8d377a3fa08c7bfd4f839a59
Steven-19-2 <88902246+Steven-19-2@users.noreply.github.com>
2023-04-25 23:49:16 +0530

    added

--- app/src/main/java/com/example/splash_screen/dashboard.java
+++ app/src/main/java/com/example/splash_screen/dashboard.java
@@ -22,16 +22,70 @@ public class dashboard extends AppCompatActivity {
         setContentView(R.layout.activity_dashboard);
 
 
-        RelativeLayout rl = (RelativeLayout) findViewById(R.id.sr1);
-        rl.setOnClickListener(new View.OnClickListener() {
+        RelativeLayout r1 = (RelativeLayout) findViewById(R.id.sr1);
+        r1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent numbersIntent = new Intent(dashboard.this, beatofficerdetails.class);
+                startActivity(numbersIntent);
+            }
+
+        }
+        );
+
+        RelativeLayout r2 = (RelativeLayout) findViewById(R.id.sr2);
+        r2.setOnClickListener(new View.OnClickListener() {
                                   @Override
                                   public void onClick(View v) {
-                                      Intent numbersIntent = new Intent(dashboard.this, beatofficerdetails.class);
+                                      Intent numbersIntent = new Intent(dashboard.this, governoffices.class);
                                       startActivity(numbersIntent);
                                   }
 
                               }
         );
 
+        RelativeLayout r3 = (RelativeLayout) findViewById(R.id.sr3);
+        r3.setOnClickListener(new View.OnClickListener() {
+                                  @Override
+                                  public void onClick(View v) {
+                                      Intent numbersIntent = new Intent(dashboard.this, educationalinstitutions.class);
+                                      startActivity(numbersIntent);
+                                  }
+
+                              }
+        );
+
+        RelativeLayout r4 = (RelativeLayout) findViewById(R.id.sr4);
+        r4.setOnClickListener(new View.OnClickListener() {
+                                  @Override
+                                  public void onClick(View v) {
+                                      Intent numbersIntent = new Intent(dashboard.this, placesworship.class);
+                                      startActivity(numbersIntent);
+                                  }
+
+                              }
+        );
+
+        RelativeLayout r5 = (RelativeLayout) findViewById(R.id.sr5);
+        r5.setOnClickListener(new View.OnClickListener() {
+                                  @Override
+                                  public void onClick(View v) {
+                                      Intent numbersIntent = new Intent(dashboard.this, guesthotelrest.class);
+                                      startActivity(numbersIntent);
+                                  }
+
+                              }
+        );
+
+        RelativeLayout r6 = (RelativeLayout) findViewById(R.id.sr6);
+        r6.setOnClickListener(new View.OnClickListener() {
+                                  @Override
+                                  public void onClick(View v) {
+                                      Intent numbersIntent = new Intent(dashboard.this, bustaxipetrol.class);
+                                      startActivity(numbersIntent);
+                                  }
+
+                              }
+        );
     }
 }
