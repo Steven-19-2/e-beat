ea35431eb9689b1a29e8fe6820b1ba913b842d79
Steven-19-2 <88902246+Steven-19-2@users.noreply.github.com>
2023-04-29 21:57:13 +0530

    added

--- app/src/main/java/com/example/splash_screen/beatofficerdetails.java
+++ app/src/main/java/com/example/splash_screen/beatofficerdetails.java
@@ -2,7 +2,11 @@ package com.example.splash_screen;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
 
 public class beatofficerdetails extends AppCompatActivity {
 
@@ -10,5 +14,21 @@ public class beatofficerdetails extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_beatofficerdetails);
+
+        Button button1 = (Button) findViewById(R.id.button);
+        button1.setOnClickListener(new View.OnClickListener() {
+                                  @Override
+                                  public void onClick(View v) {
+                                      Intent numbersIntent = new Intent(beatofficerdetails.this,details.class);
+                                      startActivity(numbersIntent);
+                                  }
+
+                              }
+        );
+
+
+
+
+
     }
 }
